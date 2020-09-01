00584a439d6d80b6bbf1f551dd5b0410a22d7a5d
floreez237 <florianlowe12@gmail.com>
2020-09-01 01:10:12 +0100

    Initial commit

---
+++ src/main/java/com/florian/restful_webservices_mvc/RestfulWebservicesMvcApplication.java
@@ -0,0 +1,13 @@
+package com.florian.restful_webservices_mvc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestfulWebservicesMvcApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RestfulWebservicesMvcApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/florian/restful_webservices_mvc/RestfulWebservicesMvcApplicationTests.java
@@ -0,0 +1,13 @@
+package com.florian.restful_webservices_mvc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestfulWebservicesMvcApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
